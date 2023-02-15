09b56f3f63512b1e9db67d12b1b0e5d8ddd24ec4
John Doe <johndoe@example.com>
2023-02-15 15:51:02 -0600

    List

--- GitandLoop/Program.cs
+++ GitandLoop/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+List<string> cities = new List<string>() {"Atlanta", "Dallas", "LA", "New York City", "Boston","Chicago","Austin"};
+
+foreach(var city in cities) {
+    Console.WriteLine(city);
+}
