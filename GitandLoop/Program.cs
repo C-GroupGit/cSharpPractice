37fe5070c96b9d6b2de88e9ac45d81df0451c7e8
Lizette Villatoro <lizettevillatoro@Lizettes-MacBook-Pro.local>
2023-02-15 16:15:05 -0600

    commit

--- GitandLoop/Program.cs
+++ GitandLoop/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+Dictionary<string, string> AnimalsList = new Dictionary<string, string>();
+AnimalsList.Add("Giraffe","Mammal");
+AnimalsList.Add("Flamingo","Bird");
+AnimalsList.Add("Turtle","Reptile");
+AnimalsList.Add("Shark","Fish");
+
+foreach(var entry in AnimalsList) {
+    Console.WriteLine($"{entry.Key} is a{entry.Value}");
+}
